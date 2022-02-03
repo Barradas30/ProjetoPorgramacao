d8a8c315bc5b4eb22a365f6c2a779a8b1204e149
Filipe Barradas <filipebarradas30@gmail.com>
2022-02-03 15:12:42 +0000

    Initial Commit

---
+++ ProjetoPorgramacao/ProjetoPorgramacaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoPorgramacaoApp.swift
+//  ProjetoPorgramacao
+//
+//  Created by Admin on 03/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoPorgramacaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
